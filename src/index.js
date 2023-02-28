4b31047d7af8568f8b9d6403acf5fc3e494fab3b
GabriellMatias <gabriel.goiasat@gmail.com>
2023-02-28 12:45:37 -0300

    :hammer: Feat: Finished CRUD project

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ app.use(express.json())
 
 
 /*Middleware */
+/*pode colocar o midleware de duas formas
+app.get('/statement', MIDLEWARE, (request, response)
+OU
+app.use(MIDLEWARE) */
 function verifyExistsAccountCPF(request, response, next) {
   /*Next define se o midleware vai continuar ou vai barrar */
   const { cpf } = request.headers
@@ -31,10 +35,19 @@ function verifyExistsAccountCPF(request, response, next) {
   return next() /*Contina a aplicacao */
 }
 
-/*pode colocar o midleware de duas formas
-app.get('/statement', MIDLEWARE, (request, response)
-OU
-app.use(MIDLEWARE) */
+/*Calculado saldo da acc */
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount
+    } else {
+       return acc - operation.amount
+    }
+  }, 0)
+  return balance
+}
+
+/*Realizando extrato bancario */
 app.get('/statement', verifyExistsAccountCPF, (request, response) => {
 
   const { customer } = request
@@ -64,6 +77,7 @@ app.post('/account', (request, response) => {
 })
 
 /* DEIXEI O REQUEST SEMPRE ANTES DO RESPONSE */
+/*Depositando */
 app.post('/deposity', verifyExistsAccountCPF, (request, response) => {
   const { description, amount } = request.body
   const { customer } = request
@@ -77,5 +91,66 @@ app.post('/deposity', verifyExistsAccountCPF, (request, response) => {
   return response.status(201).send({ message: 'Desposity made successfully' })
 })
 
+/*Realizando Saque */
+app.post('/withdraw', verifyExistsAccountCPF, (request, response) => {
+  const { amount } = request.body
+  const { customer } = request
+
+  const balance = getBalance(customer.statment)
+
+  if (balance < amount) {
+    return response.status(400).json({ error: 'Insufficient funds' })
+  }
+  const statementOperation = {
+    amount,
+    createdAt: new Date(),
+    type: 'debit'
+  }
+  customer.statment.push(statementOperation)
+
+  return response.status(201).send({ message: 'Withdrawal made successfully' })
+
+})
+
+/* Pegando extrato pela data*/
+app.get('/statement/date', verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request
+  const { date } = request.query
+  const dateFormated = new Date(date + ' 00:00')
+  const statement = customer.statment.filter(statement => statement.createdAt.toDateString() === new Date(dateFormated).toDateString())
+
+  return response.status(200).json(statement)
+})
+
+/*Modificando nome da conta */
+app.put('/account', verifyExistsAccountCPF, (request, response) => {
+  const { name } = request.body
+  const { customer } = request
+  customer.name = name
+  return response.status(200).send({ message: 'Changes made successfully' })
+})
+
+/*Printando dados do cliente */
+app.get('/account', verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request
+  return response.status(200).json(customer)
+})
+
+/*Deletando acc */
+app.delete('/account', verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request
+  customers.splice(customer, 1)
+
+  return response.status(200).send({ message: 'Account deleted successfully', customers })
+})
+
+
+app.get("/balance", verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request
+  const balance = getBalance(customer.statment)
+
+  return response.json(balance)
+
+})
 
 app.listen(3333)
